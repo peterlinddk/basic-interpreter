751544faca4f555244d53ebfef59cf293231c7d1
Peter Lind <github@peterlind.dk>
2025-07-12 12:14:15 +0200

    first version of PRINT

--- basic.c
+++ basic.c
@@ -3,6 +3,9 @@
 #include <ctype.h>
 
 int startsWithIgnoreCase(char *string, const char *word);
+void kw_let(char *parameter);
+void kw_print(char *parameter);
+void kw_input(char *parameter);
 
 const char *keywords[] = {"NEW", "LIST", "RUN", "END", "REM", "LET", "PRINT", "INPUT"};
 
@@ -33,6 +36,23 @@ int main()
       if (startsWithIgnoreCase(inp, keywords[kw]))
       {
         printf("Recognized keyword: %s in %s\n", keywords[kw], inp);
+        switch (kw)
+        {
+        case 5:
+          kw_let(inp + strlen(keywords[kw]));
+          break;
+        case 6:
+          kw_print(inp + strlen(keywords[kw]));
+          break;
+        case 7:
+          kw_input(inp + strlen(keywords[kw]));
+
+        default:
+          break;
+        }
+
+        // don't look at any more keywords
+        break;
       }
     }
 
@@ -52,4 +72,40 @@ int startsWithIgnoreCase(char *string, const char *word)
     }
   }
   return 1;
+}
+
+void kw_let(char *parm)
+{
+  printf("LET %s\n", parm);
+}
+
+void kw_print(char *parm)
+{
+  // ignore whitespace in parameter
+  while (*parm == ' ' || *parm == '\t')
+      parm++;
+
+  printf("PRINT %s\n", parm);
+
+  // find start and end of string
+  char* start = parm;
+  while(*start != '"') start++;
+  start++;
+  char* end = start;
+  while(*end != '"') end++;
+
+  // print string between start and end
+  *end = '\0';
+  printf("String: %s", start);
+/*  while(start < end)
+  {
+    putchar(*start);
+    start++;
+  }
+    */
+}
+
+void kw_input(char *parm)
+{
+  printf("INPUT %s\n", parm);
 }
