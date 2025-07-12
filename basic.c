2e67225defb889629a00b0cb3335c52bc9bd5f68
Peter Lind <github@peterlind.dk>
2025-07-12 17:33:16 +0200

    added variables - store only

--- basic.c
+++ basic.c
@@ -5,6 +5,11 @@
 int startsWithIgnoreCase(char *string, const char *word);
 char escapeChar(char c);
 
+void variables_dump();
+void variable_create(char *name, char *value);
+void variable_write(char *name, char *newvalue);
+char *variable_read(char *name);
+
 void kw_let(char *parameter);
 void kw_print(char *parameter);
 void kw_input(char *parameter);
@@ -58,7 +63,7 @@ int main()
       }
     }
 
-    printf("You entered '%s'\n", inputline);
+//    printf("You entered '%s'\n", inputline);
   } while (!startsWithIgnoreCase(inputline, "QUIT"));
 
   return 0;
@@ -76,6 +81,43 @@ int startsWithIgnoreCase(char *string, const char *word)
   return 1;
 }
 
+typedef struct
+{
+  char *name;
+  char *value;
+} variable;
+
+variable variables[128];
+int variables_used = 0;
+
+void variables_dump()
+{
+  for(int i=0; i < variables_used; i++)
+  {
+    variable *v = &variables[i];
+    printf("Variable #%d @%p - has name: \"%s\" and value: _%s_\n", i, v, v->name, v->value);
+    printf(" - name@%p and value@%p\n", v->name, v->value);
+  }
+}
+
+void variable_create(char *name, char *value)
+{
+  // create new struct, and add to the list
+  variable v = {.name = strdup(name), .value=strdup(value)};
+  // NOTE: I have a feeling that I am creating one struct here, and then copying to another inside the array ...
+  printf("variable created at address: %p\n", &variables[variables_used]);
+  variables[variables_used++] = v;
+}
+
+void variable_write(char *name, char *newvalue)
+{
+}
+
+char *variable_read(char *name)
+{
+  return "null";
+}
+
 void kw_let(char *parm)
 {
   printf("LET %s\n", parm);
@@ -90,12 +132,12 @@ void kw_let(char *parm)
   while (*parm == ' ' || *parm == '\t')
     parm++;
   // put everything before the = into name
-  while(*parm != '=')
+  while (*parm != '=')
   {
     *nam++ = *parm++;
   }
   // remove trailing whitespace from name
-  while(*(nam-1) == ' ' || *(nam-1) == '\t')
+  while (*(nam - 1) == ' ' || *(nam - 1) == '\t')
   {
     nam--;
   }
@@ -106,19 +148,22 @@ void kw_let(char *parm)
   while (*parm == ' ' || *parm == '\t')
     parm++;
   // and put everything after = into value
-  while(*parm != '\0')
+  while (*parm != '\0')
   {
     *val++ = *parm++;
   }
   // but remove trailing whitespace from value
-  while(*(val-1) == ' ' || *(val-1) == '\t')
+  while (*(val - 1) == ' ' || *(val - 1) == '\t')
   {
     val--;
   }
   *val = '\0';
 
   // print variable name and value
-  printf("LET variable: _%s_ be value: _%s_\n", name, value);
+//  printf("LET variable: _%s_ be value: _%s_\n", name, value);
+  // create variable
+  variable_create(name, value);
+  variables_dump();
 }
 
 void kw_print(char *parm)
@@ -134,7 +179,8 @@ void kw_print(char *parm)
 
   // find start of string in input
   char *input = parm;
-  while (*input++ != '"');  
+  while (*input++ != '"')
+    ;
   input++; // ignore the first "
 
   // add each character to the string
