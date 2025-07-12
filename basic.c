245ae0f44747daf9127d11524adc1f1c528b7622
Peter Lind <github@peterlind.dk>
2025-07-12 11:13:02 +0200

    added list of keywords

--- basic.c
+++ basic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+int startsWithIgnoreCase(char *string, const char *word);
+
+const char *keywords[] = {"NEW", "LIST", "RUN", "END", "REM", "LET", "PRINT", "INPUT"};
 
 int main()
 {
@@ -15,8 +20,36 @@ int main()
     // remove last newline
     inputline[strlen(inputline) - 1] = '\0';
 
+    // check if the inputstring contains a keyword
+    char *inp = inputline;
+
+    // ignore leading whitespace
+    while (*inp == ' ' || *inp == '\t')
+      inp++;
+
+    // compare input with each keyword in list
+    for (int kw = 0; kw < 8; kw++)
+    {
+      if (startsWithIgnoreCase(inp, keywords[kw]))
+      {
+        printf("Recognized keyword: %s in %s\n", keywords[kw], inp);
+      }
+    }
+
     printf("You entered '%s'\n", inputline);
-  } while (strncmp(inputline, "QUIT", 4) != 0);
+  } while (!startsWithIgnoreCase(inputline, "QUIT"));
 
   return 0;
+}
+
+int startsWithIgnoreCase(char *string, const char *word)
+{
+  while (*word)
+  {
+    if (toupper(*string++) != *word++)
+    {
+      return 0;
+    }
+  }
+  return 1;
 }
